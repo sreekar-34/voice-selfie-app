2a6efd63ae99813e73c1d6616660d6604e9a261c
sreekar-34 <95081023+sreekar-34@users.noreply.github.com>
2022-11-21 16:31:11 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -12,6 +12,26 @@ recognition.onresult=function(event){
    console.log(event);
    var Content=event.results[0][0].transcript;
    document.getElementById("textbox").innerHTML= Content;  
+   speak();
+   Webcam.attach(camera);
 }
 
+
+function speak(){
+    var synth = window.speechSynthesis;
+    speakdata = document.getElementById("textbox").value;
+    var utterThis = new SpeechSynthesisUtterance(speakdata);
+    synth.speak(utterThis);
+}
+
+
+Webcam.set({
+    width:360,
+    height:250,
+    image_format:'png',
+    png_quality:90
+});
+
+
+camera = document.getElementById("camera");
 
